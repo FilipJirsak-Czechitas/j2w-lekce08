f3199073c748668c0ba5e626061ae7eb0edfeb7a
Filip Jirsák <filip@jirsak.org>
2024-05-26 22:32:13 +0200

    Aktualizace jaro 2024 – Java 21, Gradle 8.7, Spring Boot 3.2.4

--- src/main/java/cz/czechitas/java2webapps/lekce8/controller/OsobaController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce8/controller/OsobaController.java
@@ -8,11 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,63 +17,63 @@ import java.util.Optional;
 @Controller
 public class OsobaController {
 
-  private final OsobaRepository osobaRepository;
+    private final OsobaRepository osobaRepository;
 
-  public OsobaController(OsobaRepository osobaRepository) {
-    this.osobaRepository = osobaRepository;
-  }
-
-  @InitBinder
-  public void nullStringBinding(WebDataBinder binder) {
-    //prázdné textové řetězce nahradit null hodnotou
-    binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
-  }
-
-  @GetMapping("/")
-  public ModelAndView seznam() {
-    return new ModelAndView("seznam")
-            .addObject("osoby", osobaRepository.findAll());
-  }
-
-  @GetMapping("/novy")
-  public ModelAndView novy() {
-    return new ModelAndView("detail")
-            .addObject("osoba", new Osoba());
-  }
-
-  @PostMapping("/novy")
-  public String pridat(@ModelAttribute("osoba") @Valid Osoba osoba, BindingResult bindingResult) {
-    if (bindingResult.hasErrors()) {
-      return "detail";
+    public OsobaController(OsobaRepository osobaRepository) {
+        this.osobaRepository = osobaRepository;
     }
-    osoba.setId(null);
-    osobaRepository.save(osoba);
-    return "redirect:/";
-  }
 
-  @GetMapping("/{id:[0-9]+}")
-  public ModelAndView detail(@PathVariable long id) {
-    Optional<Osoba> osoba = osobaRepository.findById(id);
-    if (osoba.isEmpty()) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    @InitBinder
+    public void nullStringBinding(WebDataBinder binder) {
+        //prázdné textové řetězce nahradit null hodnotou
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
-    return new ModelAndView("detail")
-            .addObject("osoba", osoba.get());
-  }
 
-  @PostMapping("/{id:[0-9]+}")
-  public String ulozit(@ModelAttribute("osoba") @Valid Osoba osoba, BindingResult bindingResult) {
-    if (bindingResult.hasErrors()) {
-      return "detail";
+    @GetMapping("/")
+    public ModelAndView seznam() {
+        return new ModelAndView("seznam")
+                .addObject("osoby", osobaRepository.findAll());
     }
-    osobaRepository.save(osoba);
-    return "redirect:/";
-  }
 
-  @PostMapping(value = "/{id:[0-9]+}", params = "akce=smazat")
-  public String smazat(@PathVariable long id) {
-    osobaRepository.deleteById(id);
-    return "redirect:/";
-  }
+    @GetMapping("/novy")
+    public ModelAndView novy() {
+        return new ModelAndView("detail")
+                .addObject("osoba", new Osoba());
+    }
+
+    @PostMapping("/novy")
+    public String pridat(@ModelAttribute("osoba") @Valid Osoba osoba, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "detail";
+        }
+        osoba.setId(null);
+        osobaRepository.save(osoba);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id:[0-9]+}")
+    public ModelAndView detail(@PathVariable long id) {
+        Optional<Osoba> osoba = osobaRepository.findById(id);
+        if (osoba.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return new ModelAndView("detail")
+                .addObject("osoba", osoba.get());
+    }
+
+    @PostMapping("/{id:[0-9]+}")
+    public String ulozit(@ModelAttribute("osoba") @Valid Osoba osoba, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "detail";
+        }
+        osobaRepository.save(osoba);
+        return "redirect:/";
+    }
+
+    @PostMapping(value = "/{id:[0-9]+}", params = "akce=smazat")
+    public String smazat(@PathVariable long id) {
+        osobaRepository.deleteById(id);
+        return "redirect:/";
+    }
 
 }
